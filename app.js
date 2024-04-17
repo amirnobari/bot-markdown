e827b1b7bb545166957e3a112a8a782903b2cfd3
amir <amirnobari1990@gmail.com>
2024-04-18 02:07:36 +0330

    chore : "update save data in json file"

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const TelegramBot = require('node-telegram-bot-api')
+const fs = require('fs')
 require('dotenv').config()
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true })
-const userMessages = {} // دیکشنری برای ذخیره متن پیام هر کاربر
+const userMessagesFile = 'userMessages.json'
+let startedUsers = []
+let helpRequested = false
+
+let userMessages = {}
+
+// بررسی وجود فایل و لود اطلاعات
+if (fs.existsSync(userMessagesFile))
+{
+    const data = fs.readFileSync(userMessagesFile)
+    userMessages = JSON.parse(data)
+}
+
+// ذخیره سازی پیام کاربر در فایل
+function saveUserMessage (userId, message)
+{
+    // استفاده از مقدار واقعی دکمه‌های مارک‌داون برای ذخیره در فایل JSON
+    if (message.startsWith('/'))
+    {
+        // اگر پیام یک دستور است، آن را به صورت یک دیکشنری با کلید "command" ذخیره کنید
+        userMessages[userId] = { command: message, markdownOption: '' }
+    } else
+    {
+        // در غیر این صورت، پیام متنی است و به عنوان "message" ذخیره شود
+        userMessages[userId] = { message: message, markdownOption: '' }
+    }
+    fs.writeFileSync(userMessagesFile, JSON.stringify(userMessages))
+}
+
+
+
+// حذف پیام کاربر از فایل
+function deleteUserMessage (userId)
+{
+    delete userMessages[userId]
+    fs.writeFileSync(userMessagesFile, JSON.stringify(userMessages))
+}
 
 async function checkPublicChannelMembership (userId)
 {
@@ -13,7 +50,7 @@ async function checkPublicChannelMembership (userId)
     {
         const [channel1Membership, channel2Membership] = await Promise.all([
             bot.getChatMember(channel1Id, userId),
-            bot.getChatMember(channel2Id, userId)
+            bot.getChatMember(channel2Id, userId),
         ])
 
         const isMember1 = channel1Membership.status !== 'left' && channel1Membership.status !== 'kicked'
@@ -27,235 +64,24 @@ async function checkPublicChannelMembership (userId)
     }
 }
 
-let showMembershipPrompt = true
-const startedUsers = [] // آرایه برای ذخیره آی دی کاربرانی که استارت زده‌اند
+bot.onText(/^\/start_new_text$/, (msg) =>
+{
+    const chatId = msg.chat.id
+    if (!startedUsers.includes(chatId))
+    {
+        startedUsers.push(chatId)
+        userMessages[chatId] = { message: '', markdownOption: '' }
+        sendInstructions(chatId)
+    }
+});
+
+bot.onText(/^\/help$/, (msg) =>
+{
+    const chatId = msg.chat.id
+    handleHelp(chatId)
+})
 
 bot.onText(/^\/start$/, (msg) =>
-{
-    const chatId = msg.chat.id
-    const userId = msg.from.id
-
-    // بررسی آیا کاربر قبلاً استارت زده یا نه
-    if (startedUsers.includes(userId))
-    {
-        // اگر قبلاً استارت زده بود، پیام مناسب را ارسال کنید
-        bot.sendMessage(chatId, 'شما قبلاً استارت زده‌اید!')
-        return
-    }
-
-    // اضافه کردن آی دی کاربر به آرایه کاربرانی که استارت زده‌اند
-    startedUsers.push(userId)
-
-    // اجرای دستور استارت برای کاربر
-    handleStart(msg, chatId)
-})
-
-
-function handleStart (msg, chatId)
-{
-    const userId = msg.from.id
-
-    if (msg.text.trim() === '/help')
-    {
-        handleHelp(chatId)
-        return
-    }
-
-    checkPublicChannelMembership(userId)
-        .then((membership) =>
-        {
-            const keyboard = { inline_keyboard: [] }
-
-            if (!membership.channel1)
-                keyboard.inline_keyboard.push([{ text: 'عضویت در کانال اول ما 1️⃣', url: 'https://t.me/DeepDevs' }])
-
-            if (!membership.channel2)
-                keyboard.inline_keyboard.push([{ text: 'عضویت در کانال دوم ما 2️⃣', url: 'https://t.me/InstaDevs' }])
-
-            if (keyboard.inline_keyboard.length > 0)
-            {
-                keyboard.inline_keyboard.push([{ text: '👉 Use Bot 👈', callback_data: 'use_bot' }])
-                if (showMembershipPrompt)
-                {
-                    bot.sendMessage(chatId, 'این کانالها مربوط به برنامه نویسی هست و ماله خودمونه 👨‍💻\nاگر میخوای از ربات استفاده کنی داخل این کانالها عضو شو 🙏\nبعد ازعضویت روی Use Bot کلیک کن و تمام 💪', { reply_markup: JSON.stringify(keyboard) })
-                    showMembershipPrompt = false
-                }
-            } else
-            {
-                sendInstructions(chatId)
-            }
-        })
-        .catch((error) =>
-        {
-            console.error('Error checking memberships:', error)
-            bot.sendMessage(chatId, 'An error occurred while checking channel memberships. Please try again later.')
-        })
-}
-
-
-let helpRequested = false
-bot.onText(/^\/help$/i, (msg) =>
-{
-    const chatId = msg.chat.id
-    const userId = msg.from.id
-
-    // فقط اگر کاربر قبلاً استارت رو زده بود، پاسخ دهید
-    if (startedUsers.includes(userId))
-    {
-        handleHelp(chatId)
-    } else
-    {
-        console.log("User didn't start yet.")
-        bot.sendMessage(chatId, 'برای استفاده از ربات، لطفاً دستور /start را ارسال کنید.')
-            .catch((error) =>
-            {
-                console.error('Error sending message to user:', error)
-            })
-    }
-})
-
-
-function handleHelp (chatId)
-{
-    if (helpRequested)
-    {
-        return
-    }
-    helpRequested = true
-    const helpMessage = `
-    به مرکز راهنمایی ربات Markdown خوش آمدید:
-
-    /start_new_text -  ♻️ متن جدیدت رو بنویس ♻️
-    /help -  🆘 دسترسی به بخش راهنمایی 🆘
-
-⚠️در این عکس تمام سینتکس های مربوط به فرمت Markdown به صورت مثالی نوشته شده است ⚠️
-    `
-    bot.sendMessage(chatId, helpMessage)
-        .then(() =>
-        {
-            const url = 'https://ibb.co/7Qx87YN'
-            bot.sendPhoto(chatId, url)
-            setTimeout(() =>
-            {
-                helpRequested = false
-            }, 1000)
-        })
-        .catch((error) => console.error('Error sending help message:', error))
-
-    const userId = chatId
-    // چک کردن آیا کاربر استارت زده یا نه
-    if (!startedUsers.includes(userId))
-    {
-        bot.sendMessage(chatId, 'برای شروع از دستور /start استفاده کنید.')
-    }
-}
-
-
-bot.on('callback_query', (query) =>
-{
-    const chatId = query.message.chat.id
-    const data = query.data
-
-    if (data === 'use_bot')
-    {
-        const userId = query.from.id
-
-        checkPublicChannelMembership(userId)
-            .then((membership) =>
-            {
-                if (membership.channel1 && membership.channel2)
-                {
-                    sendInstructions(chatId)
-                } else
-                {
-                    let message = 'کانالهای ما مربوط به برنامه نویسه و ماله خودمون هست 👨‍💻\n\nاگر میخوای از ربات استفاده کنی داخل کانالهامون عضو شو 🙏\n\nبعد ازعضویت روی use bot کلیک کن و تمام 💪\n\n'
-                    if (!membership.channel1) message += '😝 به کانال اول ما هنوز عضو نشدی  😝\n'
-                    if (!membership.channel2) message += '😝 به کانال دوم ما هنوز عضو نشدی  😝\n'
-
-                    if (message.trim() !== '') bot.sendMessage(chatId, message)
-                    else console.error('Empty message')
-                }
-            })
-            .catch((error) =>
-            {
-                console.error('Error checking memberships:', error)
-                bot.sendMessage(chatId, 'An error occurred while checking channel memberships. Please try again later.')
-            })
-    } else
-    {
-        handleMarkdownQuery(query)
-    }
-})
-
-function handleMarkdownQuery (query)
-{
-    const chatId = query.message.chat.id
-    const data = query.data
-    let formattedText = ''
-
-    if (data === '/help')
-    {
-        handleHelp(chatId)
-        return
-    }
-
-    // استفاده از دیکشنری userMessages برای متن مورد نظر کاربر
-    const temporaryText = userMessages[chatId] || ''
-
-    if (data === 'new_text')
-    {
-        sendInstructions(chatId) // ارسال دستور برای وارد کردن متن جدید
-        return
-    }
-
-    // اگر کاربر بجای انتخاب متن جدید، دوباره از گزینه‌های مارک‌داون موجود استفاده کند، به او یک یادآوری ارسال شود
-    if (temporaryText === '' && data !== 'new_text')
-    {
-        bot.sendMessage(chatId, 'متن جدیدت رو وارد نکردی هنوز ✍️').catch(console.error)
-        return
-    }
-
-    switch (data)
-    {
-        case 'bold':
-            formattedText = `**${temporaryText}**`
-            break
-        case 'italic':
-            formattedText = `_${temporaryText}_`
-            break
-        case 'code':
-            formattedText = `\`${temporaryText}\``
-            break
-        case 'link':
-            sendLinkPrompt(chatId)
-            return
-        case 'header_1':
-        case 'header_2':
-        case 'header_3':
-        case 'header_4':
-            const level = parseInt(data.split('_')[1])
-            formattedText = `${'#'.repeat(level)} ${temporaryText}`
-            break
-        case 'inline_code':
-            formattedText = `\`${temporaryText}\``
-            break
-        case 'strikethrough':
-            formattedText = `~~${temporaryText}~~`
-            break
-        case 'quote':
-            formattedText = `> ${temporaryText}`
-            break
-        case 'item list':
-            formattedText = `- ${temporaryText}`
-            break
-    }
-
-    if (formattedText.trim() !== '')
-    {
-        bot.sendMessage(chatId, formattedText)
-    }
-}
-bot.onText(/^\/start_new_text$/, (msg) =>
 {
     const chatId = msg.chat.id
     sendInstructions(chatId)
@@ -264,36 +90,37 @@ bot.onText(/^\/start_new_text$/, (msg) =>
 
 function sendInstructions (chatId)
 {
-    userMessages[chatId] = '' // انتخاب یک متغیر مجزا برای هر کاربر
-    bot.sendMessage(chatId, 'متن مورد نظرت رو بنویس تا من برات قالب های Markdown رو نشون بدم ✍️')
-        .then(() =>
+    bot.sendMessage(chatId, 'متن مورد نظرت رو بنویس تا من برات قالب های Markdown رو نشون بدم ✍️').then(() =>
+    {
+        bot.once('message', (msg) =>
         {
-            bot.once('message', (msg) =>
+            if (msg.chat.id === chatId)
             {
                 if (msg.text.trim() === '/help')
                 {
                     handleHelp(chatId)
                 } else if (msg.text.trim() === '')
                 {
-                    // اگر پیام متنی نبود ولی شامل عکس یا سند بود
                     if (msg.photo !== undefined || msg.document !== undefined)
                     {
-                        // به کاربر اخطار داده شود
                         bot.sendMessage(chatId, 'فقط می‌توانید متن ارسال کنید.')
                     } else
                     {
-                        // اگر پیام متنی نبود و همچنین شامل عکس یا سند نبود، به کاربر پیام خطایی داده شود
                         bot.sendMessage(chatId, 'خطای نامشخص رخ داده است. لطفاً دوباره تلاش کنید.')
                     }
                 } else
                 {
-                    userMessages[chatId] = msg.text // ذخیره متن پیام کاربر در دیکشنری userMessages
+                    userMessages[chatId].message = msg.text
                     sendMarkdownOptions(chatId)
                 }
-            })
+            }
         })
+    })
 }
 
+
+
+// Function to send markdown options
 function sendMarkdownOptions (chatId)
 {
     const userId = chatId
@@ -356,13 +183,152 @@ function sendMarkdownOptions (chatId)
         })
 }
 
+bot.on('message', (msg) =>
+{
+    const chatId = msg.chat.id
+    saveUserMessage(chatId, msg.text)
+})
+
+// Callback query handling
+bot.on('callback_query', (query) =>
+{
+    const chatId = query.message.chat.id
+    const data = query.data
+
+    if (data === 'use_bot')
+    {
+        sendInstructions(chatId)
+    } else
+    {
+        // Save the selected markdown option for the user
+        userMessages[chatId].markdownOption = data
+        handleMarkdownQuery(query)
+    }
+});
+
+
+function handleMarkdownQuery (query)
+{
+    const chatId = query.message.chat.id
+    const data = query.data
+    let formattedText = ''
+
+    if (data === '/help')
+    {
+        handleHelp(chatId)
+        return
+    }
+
+    const temporaryText = userMessages[chatId] ? userMessages[chatId].message : ''
+
+    if (data === 'new_text')
+    {
+        sendInstructions(chatId)
+        return
+    }
+
+    if (temporaryText === '' && data !== 'new_text')
+    {
+        bot.sendMessage(chatId, 'متن جدیدت رو وارد نکردی هنوز ✍️').catch(console.error)
+        return
+    }
+
+    switch (data)
+    {
+        case 'bold':
+            formattedText = `**${temporaryText}**`
+            break
+        case 'italic':
+            formattedText = `_${temporaryText}_`
+            break
+        case 'code':
+            formattedText = `\`${temporaryText}\``
+            break
+        case 'link':
+            sendLinkPrompt(chatId)
+            return
+        case 'header_1':
+        case 'header_2':
+        case 'header_3':
+        case 'header_4':
+            const level = parseInt(data.split('_')[1])
+            formattedText = `${'#'.repeat(level)} ${temporaryText}`
+            break
+        case 'inline_code':
+            formattedText = `\`${temporaryText}\``
+            break
+        case 'strikethrough':
+            formattedText = `~~${temporaryText}~~`
+            break
+        case 'quote':
+            formattedText = `> ${temporaryText}`
+            break
+        case 'item list':
+            formattedText = `- ${temporaryText}`
+            break
+    }
+
+    if (formattedText.trim() !== '')
+    {
+        bot.sendMessage(chatId, formattedText)
+    }
+
+    // Update userMessages JSON file with the latest markdown option
+    fs.writeFileSync(userMessagesFile, JSON.stringify(userMessages))
+}
+
 function sendLinkPrompt (chatId)
 {
-    bot.sendMessage(chatId, '🔗 آدرس URL ات رو وارد کن 🔗')
-    bot.once('message', (msg) =>
+    bot.sendMessage(chatId, '🔗 آدرس URL ات رو وارد کن 🔗').then(() =>
     {
-        const url = msg.text
-        const formattedText = `[${userMessages[chatId]}](${url})` // استفاده از userMessages برای متن مورد نظر کاربر
-        bot.sendMessage(chatId, formattedText)
+        bot.once('message', (msg) =>
+        {
+            const url = msg.text.trim() // دریافت آدرس URL از پیام
+            const temporaryText = userMessages[chatId].message // متن مورد نظر کاربر
+            const formattedText = `[${temporaryText}](${url})` // ساخت متن لینک
+            bot.sendMessage(chatId, formattedText) // ارسال متن لینک به کاربر
+            // Update userMessages JSON file with the latest markdown option
+            userMessages[chatId].markdownOption = 'link'
+            fs.writeFileSync(userMessagesFile, JSON.stringify(userMessages))
+        })
     })
 }
+
+
+
+
+function handleHelp (chatId)
+{
+    if (helpRequested)
+    {
+        return
+    }
+    helpRequested = true
+    const helpMessage = `
+    به مرکز راهنمایی ربات Markdown خوش آمدید:
+
+    /start_new_text -  ♻️ متن جدیدت رو بنویس ♻️
+    /help -  🆘 دسترسی به بخش راهنمایی 🆘
+
+⚠️در این عکس تمام سینتکس های مربوط به فرمت Markdown به صورت مثالی نوشته شده است ⚠️
+    `
+    bot.sendMessage(chatId, helpMessage)
+        .then(() =>
+        {
+            const url = 'https://ibb.co/7Qx87YN'
+            bot.sendPhoto(chatId, url)
+            setTimeout(() =>
+            {
+                helpRequested = false
+            }, 1000)
+        })
+        .catch((error) => console.error('Error sending help message:', error))
+
+    const userId = chatId
+    // چک کردن آیا کاربر استارت زده یا نه
+    if (!startedUsers.includes(userId))
+    {
+        bot.sendMessage(chatId, 'برای شروع از دستور /start استفاده کنید.')
+    }
+}
+
